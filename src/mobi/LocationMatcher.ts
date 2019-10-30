26e61bc5207cf57bfaca25aed7c4c82b2ec66219
quirell <maxiol3@yahoo.com>
2019-10-30 21:22:56 +0900

    Refactor Location matcher a little

--- src/mobi/LocationMatcher.ts
+++ src/mobi/LocationMatcher.ts
@@ -10,55 +10,58 @@ function firstValidUtf8ByteAfter(pos: number, buffer: ArrayBuffer) {
     return pos;
 }
 
+const whitespace_betweenRtTag_htmlTag = /(<rt>.*<\/rt>)|(<[^<]*>)|([\s　]+)/g;
 const utfEncoder = new TextEncoder();
 const utfDecoder = new TextDecoder();
 
+
+function foundByteStartIndex(foundIndex: number, htmlSearchExcerpt: string) {
+    let excerptFoundStartIndex = 0;
+    let startIndexAfterFoundIndex = 0;
+    let match: RegExpExecArray;
+
+    while (startIndexAfterFoundIndex <= foundIndex) {
+        // Token is never null, because condition will be met after the last match in the worst case
+        match = whitespace_betweenRtTag_htmlTag.exec(htmlSearchExcerpt)!;
+        startIndexAfterFoundIndex += match.index! - excerptFoundStartIndex;
+        excerptFoundStartIndex = whitespace_betweenRtTag_htmlTag.lastIndex
+    }
+    // Remove length of the last html match (or whitespaces)
+    excerptFoundStartIndex -= match![0].length;
+    //  startIndexAfterFoundIndex is equal or greater than actualFoundIndex
+    //  so we need to subtract that length |>(  )foundIndex.(thatLength).startIndexAfterFoundIndex<  |
+    // to get global excerpt found start index
+    excerptFoundStartIndex -= (startIndexAfterFoundIndex - foundIndex);
+    return utfEncoder.encode(htmlSearchExcerpt.slice(0, excerptFoundStartIndex)).length;
+}
+
+
 export function findHighlightByteIndex(highlight: Clipping, bookBinary: ArrayBuffer, locations: number) {
-    const whitespace_betweenRtTag_htmlTag = /(<rt>.*<\/rt>)|(<[^<]*>)|([\s　]+)/g;
     const SEARCH_RADIUS = 1000;
     const MAX_SEARCH_RADIUS = 3000;
 
-    let currentRadius = 0;
-    let foundIndex = -1;
     const locationSize = bookBinary.byteLength / locations;
     const locationStart = highlight.location!.start * locationSize;
     const sanitizedContent = highlight.content.replace(/[\s　]+/g, "");
+    let currentRadius = 0;
+    let foundIndex = -1;
     let htmlSearchExcerpt: string;
-    let searchExcerptBinary: ArrayBuffer;
+    let searchExcerptByteStart;
     do {
         currentRadius += SEARCH_RADIUS;
-        searchExcerptBinary = bookBinary.slice(locationStart - currentRadius, locationStart + currentRadius);
-        const validSearchExcerptBinary = searchExcerptBinary.slice(
-            firstValidUtf8ByteAfter(0, searchExcerptBinary),
-            // max length of utf8 is 4, therefore 5 is a safe value
-            firstValidUtf8ByteAfter(searchExcerptBinary.byteLength - 5, searchExcerptBinary));
+        searchExcerptByteStart = firstValidUtf8ByteAfter(locationStart - currentRadius, bookBinary);
+        // max byte length of utf8 is 4
+        const searchExcerptByteEnd = firstValidUtf8ByteAfter(locationStart + currentRadius - 4, bookBinary);
+        const searchExcerptBinary = bookBinary.slice(searchExcerptByteStart, searchExcerptByteEnd);
 
-        htmlSearchExcerpt = utfDecoder.decode(validSearchExcerptBinary);
+        htmlSearchExcerpt = utfDecoder.decode(searchExcerptBinary);
         const searchExcerpt = htmlSearchExcerpt.replace(whitespace_betweenRtTag_htmlTag, "");
 
         foundIndex = searchExcerpt.search(sanitizedContent);
     } while (foundIndex === -1 && currentRadius !== MAX_SEARCH_RADIUS);
 
     if (foundIndex > -1) {
-        let excerptFoundStartIndex = 0;
-        let localFoundStartIndex = 0;
-        let token: RegExpExecArray;
-        while (localFoundStartIndex <= foundIndex) {
-            // Token is never null, because condition will be met after the last match in the worst case
-            token = whitespace_betweenRtTag_htmlTag.exec(htmlSearchExcerpt)!;
-            localFoundStartIndex += token.index! - excerptFoundStartIndex;
-            excerptFoundStartIndex = whitespace_betweenRtTag_htmlTag.lastIndex
-        }
-        // Remove length of the last token
-        excerptFoundStartIndex -= token![0].length;
-        //  localFoundStartIndex is equal or greater than actualFoundIndex
-        //  so we need to subtract that length |>(  )foundIndex.(thatLength).localFoundStartIndex<  |
-        // to get global excerpt found start index
-        excerptFoundStartIndex -= localFoundStartIndex - foundIndex;
-        const globalExcerptByteStartIndex = locationStart - currentRadius + firstValidUtf8ByteAfter(0, searchExcerptBinary);
-        const excerptFoundByteStartIndex = utfEncoder.encode(htmlSearchExcerpt.slice(0, excerptFoundStartIndex)).length;
-        const globalFoundByteStartIndex = globalExcerptByteStartIndex + excerptFoundByteStartIndex;
-        return globalFoundByteStartIndex;
+        return searchExcerptByteStart + foundByteStartIndex(foundIndex, htmlSearchExcerpt);
     }
     return -1;
 }
